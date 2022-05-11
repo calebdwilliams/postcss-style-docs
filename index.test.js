6968c815ddbe160f8ea0ebe99523d7e48caa82a7
Caleb D. Williams <caleb.d.williams@gmail.com>
2022-05-10 23:17:38 -0500

    feat: add initial files

--- index.test.js
+++ index.test.js
@@ -1,17 +1,139 @@
-const postcss = require('postcss')
+const postcss = require('postcss');
+const postcssNesting = require('postcss-nesting');
+const { getDocsMessage, styleDocsPlugin, pluginName } = require('./lib/cjs/index');
 
-const plugin = require('./')
+async function process(input, options = {}) {
+  const result = await postcss([
+    styleDocsPlugin(options),
+    postcssNesting(),
+  ]).process(input, { from : undefined });
 
-async function run (input, output, opts = { }) {
-  let result = await postcss([plugin(opts)]).process(input, { from: undefined })
-  expect(result.css).toEqual(output)
-  expect(result.warnings()).toHaveLength(0)
+  expect(result.warnings()).toHaveLength(0);
+  return result;
 }
 
-/* Write tests here
+const defaultInput = `
+/**
+ * @docs
+ * Simple class for styling text
+ * might be multiple lines
+ */
+.text {
+  color: #141414;
+}`;
 
-it('does something', async () => {
-  await run('a{ }', 'a{ }', { })
+const defaultOutput = `
+.text {
+  color: #141414;
+}`
+
+it('will remove @docs comments', async () => {
+  const { css } = await process(defaultInput);
+  expect(css).toEqual(defaultOutput);
+});
+
+it('will emit a docs map', async () => {
+  const result = await process(defaultInput);
+  const docs = result.messages.filter(message => message.plugin === pluginName);
+  const { commentMap } = docs[0];
+
+  expect(docs.length).toEqual(1);
+  expect(commentMap.size).toEqual(1);
+  expect(commentMap.get('.text')).toEqual('Simple class for styling text might be multiple lines');
+});
+
+it('will run with default options', async () => {
+  const result = await postcss([
+    styleDocsPlugin()
+  ]).process(defaultInput, { from: undefined });
+
+  const docs = result.messages.filter(message => message.plugin === pluginName);
+  const { commentMap } = docs[0];
+
+  expect(result.css).toEqual(defaultOutput);
+  expect(commentMap.size).toEqual(1);
+  expect(commentMap.get('.text')).toEqual('Simple class for styling text might be multiple lines');
+});
+
+it('will preserve comments if the preserve option is set to true', async () => {
+  const result = await process(defaultInput, { preserve: true });
+  expect(result.css).toEqual(defaultInput);
+});
+
+it('will do nothing to comments not describing rules', async () => {
+  const result = await process(`
+  /** @docs Default text comment */
+  .text {
+    /** This is my favorite color */
+    color: tomato;
+  }`, { preserve: false });
+
+  expect(result.css).toEqual(`
+  .text {
+    /** This is my favorite color */
+    color: tomato;
+  }`);
+
+  const docs = result.messages.filter(message => message.plugin === pluginName);
+  const { commentMap } = docs[0];
+
+  expect(commentMap.size).toEqual(1);
+});
+
+it('will preserve whitespaces if cleanWhitespaces is false', async () => {
+  const { messages } = await process(defaultInput, { cleanWhitespace: false });
+  const message = getDocsMessage(messages);
+
+  expect(message.commentMap.get('.text')).toEqual(`Simple class for styling text
+  might be multiple lines`);
+});
+
+it('will ignore comments that do not use the comment string', async () => {
+  const undocumentedInput = `
+  /**
+   * Simple class for styling text
+   * might be multiple lines
+   */
+  .text {
+    color: #141414;
+  }
+  `;
+  const { messages, css } = await process(undocumentedInput);
+
+  expect(css).toEqual(undocumentedInput);
+  expect(getDocsMessage(messages).commentMap.size).toEqual(0);
+});
+
+it('will work with postcss-nesting', async () => {
+  const nestedInput = `
+  /**
+   * @docs
+   * Simple class for styling text
+   * might be multiple lines
+   */
+  .nested-text {
+    color: #141414;
+
+    /**
+     * @docs Make it pop
+     */
+    &.nested-text--bold {
+      font-weight: 600;
+    }
+  }`;
+
+  const { css, messages } = await process(nestedInput);
+  const { commentMap } = getDocsMessage(messages);
+
+  expect(css).toEqual(`
+  .nested-text {
+    color: #141414
+  }
+.nested-text.nested-text--bold {
+      font-weight: 600;
+    }`);
+
+  expect(commentMap.size).toEqual(2);
+  expect(commentMap.has('.nested-text')).toBe(true);
+  expect(commentMap.has('.nested-text.nested-text--bold')).toBe(true);
 })
-
-*/
---
+++ index.ts
@@ -0,0 +1,132 @@
+import type { Comment, Message, Plugin, Root, Rule } from 'postcss';
+
+/** Clean comments text */
+function cleanComment(
+  text: string,
+  commentString: string,
+  cleanWhitespace: boolean
+): string {
+  let newText = text
+    .replace(/\*/gi, '')
+    .replace(commentString, '')
+    .trim();
+
+  if (cleanWhitespace) {
+    newText = newText.replace(/\s+/g, ' ');
+  }
+
+  return newText;
+}
+
+/** Create a map of selectors to comments */
+const commentMap = new Map<string, string>();
+
+export const pluginName = 'postcss-style-docs';
+
+export interface StyleDocsOptions {
+  /**
+   * The string that signifies a comment should be treated as a doc block.
+   * @default '@docs'
+   */
+  commentString?: string;
+
+  /**
+   * If set to true the plugin will remove whitespace from comment text
+   * @default true
+   */
+  cleanWhitespace?: boolean;
+
+  /**
+   * When set to true the plugin will preserve comments in place;
+   * otherwise, comments with the commentString sigil will be removed.
+   * @default false
+   */
+  preserve?: boolean;
+}
+
+export interface StyleDocsMessage extends Message {
+  commentMap: Map<string, string>
+  type: 'documentation',
+  plugin: 'postcss-style-docs'
+}
+
+/** The default options for the plugin */
+const defaultOptions: StyleDocsOptions = {
+  commentString: '@docs',
+  cleanWhitespace: true,
+  preserve: false
+};
+
+/**
+ * A PostCSS plugin for generating style documentation from source code
+ */
+export const styleDocsPlugin = (opts: StyleDocsOptions = {}): Plugin => {
+  const options = Object.assign(defaultOptions, opts);
+  const { cleanWhitespace, commentString, preserve } = options;
+
+  return {
+    postcssPlugin: pluginName,
+    Once(root: Root): void {
+      /**
+       * Clear any comments that might be left over
+       * from a previous run of the plugin
+       */
+      commentMap.clear();
+
+      /**
+       * Walking comments in root to make sure the plugin works
+       * well with postcss-nesting which modifies the AST before
+       * this plugin has a chance to get around to it
+       */
+      root.walkComments(comment => {
+        if (comment.text.includes(commentString)) {
+          const text = cleanComment(comment.text, commentString, cleanWhitespace);
+
+          /** The rule the comment will be documenting */
+          const next = comment.next() as Rule;
+
+          if (next && next.type === 'rule' && next.selector) {
+            let { selector } = next;
+
+            /** Special case for postcss-nesting */
+            if (next.selector.startsWith('&')) {
+              /** Loop over parents to normalize the results */
+              let parent = next.parent as Rule;
+              while (parent.type === 'rule') {
+                const safeSelector = selector.replace('&', '');
+                selector = `${parent.selector}${safeSelector}`;
+                console.log({selector})
+                parent = parent.parent as Rule;
+              }
+            }
+
+            /** Set the selector and the text into the commentMap */
+            commentMap.set(selector, text);
+          }
+
+          /**
+           * By default we want to remove these blocks to reduce the amount
+           * of code shipped to end users. This can be disabled using by
+           * setting `preserve` to `true`.
+           */
+          if (!preserve) {
+            comment.remove();
+          }
+        }
+      });
+    },
+    OnceExit(root: Root, { result }) {
+      const message: StyleDocsMessage = {
+        commentMap,
+        type: 'documentation',
+        plugin: pluginName
+      };
+
+      result.messages.push(message);
+    }
+  };
+};
+
+export function getDocsMessage(messages: Message[]): StyleDocsMessage {
+  return messages.find(message => message.plugin === pluginName) as StyleDocsMessage;
+}
